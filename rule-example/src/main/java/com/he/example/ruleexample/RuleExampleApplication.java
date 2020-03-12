e5fb6c12cade881e1d306921301a337011b58bf0
he <455450091@qq.com>
2020-03-12 18:26:58 +0800

    添加样例

---
+++ rule-example/src/main/java/com/he/example/ruleexample/RuleExampleApplication.java
@@ -0,0 +1,13 @@
+package com.he.example.ruleexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RuleExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RuleExampleApplication.class, args);
+    }
+
+}
---
+++ rule-example/src/test/java/com/he/example/ruleexample/RuleExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.he.example.ruleexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RuleExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
